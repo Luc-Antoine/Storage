a25e6dc9cbf7b5dbf2b233b9a503d21640cf9f3e
Luc-Antoine <lucantoine.dupont@gmail.com>
2019-11-21 22:24:12 +0100

    begin MVVM for Edit

---
+++ Storage/EditView/EditViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  EditViewDelegate.swift
+//  Storage
+//
+//  Created by Luc-Antoine Dupont on 21/11/2019.
+//  Copyright © 2019 Luc-Antoine Dupont. All rights reserved.
+//
+
+import Foundation
+
+protocol EditViewDelegate: AnyObject {
+    
+}
---
+++ Storage/EditView/EditViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EditViewModel.swift
+//  Storage
+//
+//  Created by Luc-Antoine Dupont on 21/11/2019.
+//  Copyright © 2019 Luc-Antoine Dupont. All rights reserved.
+//
+
+import Foundation
+
+struct EditViewModel {
+    
+}
